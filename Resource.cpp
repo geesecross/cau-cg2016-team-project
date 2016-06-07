58925f64abc40ad70663537f609d62d50827ce5b
minor5pm <minor5pm@gmail.com>
2016-06-07 20:49:13 +0900

    Trial with FreeImage

--- Resource.cpp
+++ Resource.cpp
@@ -51,12 +51,15 @@ namespace Resource {
 			SOIL_CREATE_NEW_ID,
 			SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y
 		));
+		/*
 		textures[Textures::Skybox] = new Texture(SOIL_load_OGL_texture(
 			"resources/skybox.jpg",
 			SOIL_LOAD_AUTO,
 			SOIL_CREATE_NEW_ID,
 			SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y
 		));
+		*/
+		textures[Textures::Skybox] = new Texture(Texture::loadTextureFromFile("resources/skybox.jpg"));
 
 		shaderPrograms[ShaderPrograms::Phong] = new SimpleIlluminationModelShaderProgram(
 			SimpleIlluminationModelShaderProgram::createPhong()
