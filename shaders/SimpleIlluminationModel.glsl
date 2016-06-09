6752a750ef746ced6012ba46425d5e7a92ff14b8
minor5pm <minor5pm@gmail.com>
2016-06-09 22:52:30 +0900

    Contrast for dark side

--- shaders/SimpleIlluminationModel.glsl
+++ shaders/SimpleIlluminationModel.glsl
@@ -5,14 +5,23 @@ vec4 simpleIlluminationModel(vec4 faceColor, vec3 eyeVector, vec3 lightVector, v
 	vec3 v = normalize(eyeVector);
 	vec3 n = normalize(normalVector);
 	vec3 r = dot(n, l) > 0.0 ? normalize((2.0 * dot(n, l) * n) - l) : vec3(0, 0, 0);
-	vec3 h = dot(n, l) > 0.0 ? normalize(l + v) : vec3(0, 0, 0);
+	//vec3 h = dot(n, l) > 0.0 ? normalize(l + v) : vec3(0, 0, 0);
 
 	//vec3 r = (2.0 * dot(n, l) * n) - l;
 
 	float ambient = 1.0;
-	float diffusion = max(0.0, dot(n, l));
+	//float diffusion = max(0.0, dot(n, l));
+	float diffusion = max(-1.0, dot(n, l));
 	float specular = pow(max(0.0, dot(v, r)), shiness);
 	//float specular = pow(max(0.0, dot(n, h)), shiness);
+
+	if(diffusion < 0.0) {
+		specular = 0.0;
+		if(diffusionRatio > 0.0)
+			diffusion *= ambientRatio / diffusionRatio;
+		else
+			diffusion = 0.0;
+	}
 
 	return vec4(faceColor.xyz * (ambientRatio * ambient + diffusionRatio * diffusion + specularRatio * specular), faceColor.w);
 }
